42944e3600c95054a8f2e66028f7f21cd23d1462
PREMADAS <premadas083@gmail.com>
2026-07-15 23:58:02 +0600

    qurbani-hat

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,21 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
+
+
+const getBaseURL = () => {
+    if (typeof window !== "undefined") {
+        return window.location.origin;
+    }
+
+    if (process.env.NEXT_PUBLIC_APP_URL) {
+        return process.env.NEXT_PUBLIC_APP_URL;
+    }
+    if (process.env.BETTER_AUTH_URL) {
+        return process.env.BETTER_AUTH_URL;
+    }
+
+    return "http://localhost:3000";
+};
+
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: typeof window !== "undefined" ? window.location.origin : undefined
-})
-export const { signIn, signUp, useSession } = createAuthClient();
+    baseURL: getBaseURL()
+});
